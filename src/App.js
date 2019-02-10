312e79379e185db3b02ca381ff3d011528d37a62
github username <angelina.w25@gmail.com>
2019-02-09 20:24:59 -0500

    fixed sammy's spelling

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hack the Solor System Inital Repo
+            Hack the Solar System Inital Repo
           </p>
           <a
             className="App-link"
